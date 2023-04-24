03db850a050d9b3f829df7c396d11390a5983448
Nico <Nicojr1111@gmail.com>
2023-04-24 22:50:37 +0100

    First commit, adding two games and read me

---
+++ Gamba-Game/gamba.js
@@ -0,0 +1,63 @@
+let Rlsync = require('readline-sync');
+let balance = 300;
+
+//calculations here need to be changed( research how the best slot machines do it then copy)
+const gamble = wagerAmount =>{
+  let multiplier;
+  let count = Math.ceil((Math.random() * 7) );
+  switch(count) {
+    case 1: case 2: case 3: case 4:
+      multiplier = 0;
+      break;
+    case 5:
+      multiplier = 2;
+      break;
+    case 6:
+      multiplier = 2.5;
+      break;
+    case 7:
+      multiplier = 5;
+      break;
+  }
+  return Math.round(multiplier * wagerAmount);
+}
+
+const playerWager = () =>{
+  let wager;
+  let gambleReturn;
+  do { wager = Rlsync.question(`$${balance} in funds. Place your bet! \n┏━━━✦❘༻༺❘✦━━━┓━━━✦❘༻༺❘✦━━━┓\n`); 
+}   while( isNaN(wager) || wager > balance || wager < 1);
+  balance -= wager;
+  gambleReturn = gamble(wager);
+  let newBalance = gambleReturn + balance;
+  if (gambleReturn > wager) {
+    newBalance += '+';
+  } else {
+    newBalance += '-';
+  }
+  return newBalance;
+}
+
+const playGame = () => {
+  if (balance === 0) {
+    console.log('You have run out of funds better luck next time :)');
+    return;
+  }
+  let balanceWithTag = playerWager();
+  balance = parseInt(balanceWithTag);
+  if (balanceWithTag[balanceWithTag.length - 1] === '+') {
+    console.log(`===============WINNNNNNNNNNNNN=================\n$$$$$$(^o^)~≪☆*CONGRATULATIONS*☆≫~(^o^)／$$$$$$\n`);
+  }
+
+  let repeatGameOrNot;
+   do { repeatGameOrNot = Rlsync.question(`New balance is $${balance}\nWould you like to keep playing? Press enter\nIf not please type in caps 'EXIT CASINO AND STOP PLAYING AND HAVING FUN'\n`);
+  }while( repeatGameOrNot !== '' && repeatGameOrNot !== 'EXIT CASINO AND STOP PLAYING AND HAVING FUN' );
+  
+  repeatGameOrNot ? console.log(`Your final balance is $${balance}! Be sure to play again!`): playGame();
+  
+}
+
+playGame();
+
+//make the maths bhind it better + variable with balance
+//add to browser
---
+++ README.md
@@ -0,0 +1,18 @@
+Mini games.
+
+As part of a personal project I'll be creating and adding small games to this repo.
+As my skills develop I plan to further develop each game and pursue more challenging ideas
+These games are avenues to explore different ideas and work on the process of creating and developing ideas.
+
+The games are written in Javascript and can be played in the command line with node.
+As I develop them further, browser compatability and simple graphics will be added too.
+
+#Current games
+
+Pick your path - The game has 3 modes of difficulty and in each mode you have to type in an option from 1-6 to try and avoid the traps. It logs how many times you pick the right path and ends if you fail.
+The gamesize and difficulty percentages can easily be changed for further building. 
+
+Gamba - The game works like a slot machine. You have a staring balance of 300 and are prompted what to wager. Your new balance is returned and you are given the option to wager again. If your balance goes to 0 you can't play anymore. 
+
+
+
